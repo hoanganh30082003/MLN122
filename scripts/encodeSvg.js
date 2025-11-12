3d3bc6c1dbc932fed4cd4d3c7a8c62f642ee40ce
haayongboyy <hn54707@gmail.com>
2025-11-12 22:56:47 +0700

    update main

---
+++ scripts/encodeSvg.js
@@ -0,0 +1,2 @@
+const svg = `<svg xmlns='http://www.w3.org/2000/svg' width='140' height='140' viewBox='0 0 140 140'><filter id='n'><feTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='2' stitchTiles='stitch'/></filter><rect width='100%' height='100%' filter='url(#n)'/></svg>`;
+console.log(Buffer.from(svg).toString('base64'));
